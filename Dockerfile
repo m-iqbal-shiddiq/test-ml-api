07bb119e6cc34ce8fa47d392699ceffbcf717b91
m-iqbal-shiddiq <muhammadiqbals.1998@gmail.com>
2024-09-23 23:36:39 +0700

    update API

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
 EXPOSE 8080
-CMD ["python", "app.py"]
+
+CMD ["gunicorn", "-b", "0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import numpy as np
-
 from flask import Flask, request, jsonify
 from sklearn.linear_model import LinearRegression
 
-
 app = Flask(__name__)
 
+# Data untuk Linear Regression (Berat vs Tinggi)
 X = np.array([[60], [65], [70], [75], [80]])
 y = np.array([160, 165, 170, 175, 180])
 
+# Melatih model Linear Regression
 model = LinearRegression()
 model.fit(X, y)
 
@@ -16,14 +16,13 @@ model.fit(X, y)
 def predict():
     try:
         data = request.get_json()
-        
+
         if 'weight' not in data:
             return jsonify({'error': 'No weight provided'}), 400
 
         weight = np.array([[float(data['weight'])]])
-
         prediction = model.predict(weight)
-
+        
         return jsonify({'predicted_height': prediction[0]})
 
     except Exception as e:
